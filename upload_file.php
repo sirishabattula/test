1de28051f121b7b011b3d730a51cf36533ce94a5
sirishabattula <57557419+sirishabattula@users.noreply.github.com>
2019-11-09 14:23:06 +0530

    Add files via upload

---
+++ upload_file.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+
+</body>
+
+</html>
+
+<script>
+
+var employeeobj = {"1","2"};
+if (employeeobj as row)
+{
+console.log('row property is present'); 
+}
+else
+{
+console.log('Designation property is not present'); 
+}
+
+</script>
